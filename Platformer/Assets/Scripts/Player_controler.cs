c0623335a554ca3d85ceb4e5de6339a059b7171b
VictoR-Piles <victorpilesdelgado@gmail.com>
2020-05-27 16:20:31 +0200

    Añadido el movimiento de salto y plataformas

--- Platformer/Assets/Scripts/Player_controler.cs
+++ Platformer/Assets/Scripts/Player_controler.cs
@@ -5,12 +5,14 @@ using UnityEngine.PlayerLoop;
 
 public class Player_controler : MonoBehaviour
 {
-	public float speed = 2f;												// Velocidad por defecto (se puede modificar desde Unity)
+	public float speed = 75f;												// Velocidad por defecto (se puede modificar desde Unity)
 	public float maxSpeed = 5f;												// Velocidad maxima por defecto (se puede modificar desde Unity)
-	public bool grounded;
-	
+	public bool grounded;													// Determinará si el Player esta colisionando con el suelo o no
+	public float jumpPower = 6.5f;											// Fuerza de salto por defecto del jugador (se puede modificar desde Unity)
+
 	private Rigidbody2D rb2d;
 	private Animator anim;
+	private bool jump;														// Determinará si el Player esta en el aire o no
 
 	// ==================== START ====================
 	void Start()
@@ -24,10 +26,23 @@ public class Player_controler : MonoBehaviour
 	{
 		anim.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));	// Accede al animador y cambia el Speed por la velocidad X actual
 		anim.SetBool("Grounded", grounded);
+		
+		if (Input.GetKeyDown(KeyCode.Space) && grounded)
+		{
+			jump = true;													// Determina que la tecla ESPACIO ha sido pulsada y el Player debe saltar
+		}
 	}
 
 	void FixedUpdate()
 	{
+		Vector3 fixedVelocity = rb2d.velocity;
+		fixedVelocity.x *= 0.75f;
+
+		if (grounded)
+		{
+			rb2d.velocity = fixedVelocity;									// Reduce la velocidad del Player cuando existe FRICCION con el suelo
+		}
+		
 		float h = Input.GetAxis("Horizontal");        						// Permite el movimiento horizontal con las teclas (A, D) o las flechas
 		rb2d.AddForce(Vector2.right * speed * h);
 
@@ -48,5 +63,17 @@ public class Player_controler : MonoBehaviour
 		{
 			rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);		// Evita que speed sea mayor que maxSpeed
 		}
+		
+		if (jump)
+		{
+			rb2d.velocity = new Vector2(rb2d.velocity.x, 0);				// Cancela la velocidad vertical para no poder realizar saltos con mas impulso en algunas plataformas
+			rb2d.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);		// Fisica que permite al Player saltar
+			jump = false;
+		}
+	}
+
+	void OnBecameInvisible()
+	{
+		transform.position = new Vector3(0, 0, 0);
 	}
 }
